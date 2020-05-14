554bdbc25cf65e01443ccc61e544146b6fd441fc
mike <boimik87@gmail.com>
2020-05-14 11:50:02 +0100

    sass implemented and works

---
+++ src/components/container/container.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import style from './container.module.sass';
+
+const Container = () => {
+    return (
+        <div className={style.mainContainer} >
+            <h1>iTech Calculator</h1>
+        </div>
+
+
+    )
+};
+
+export default Container;
